9a5d71f02ad3f4350c630f77098c9351f9994704
skyhill2003 <skyhill2003@gmail.com>
2022-01-14 22:34:01 +0100

    Initial commit

---
+++ src/tk/skyhill2003/programdownloadassistant/main/MainApp.java
@@ -0,0 +1,35 @@
+package tk.skyhill2003.programdownloadassistant.main;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainApp extends JFrame {
+    private JButton button1;
+    private JPanel mainPanel;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("App");
+        frame.setContentPane(new MainApp().mainPanel);
+        frame.setTitle("ProgrammDownloadAssistant");
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+        frame.setResizable(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public MainApp() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
